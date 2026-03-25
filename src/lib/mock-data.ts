9e8e06b0630b52b051be08b5984b28b6dbe46b60
Anye1990 <angiemenchaca20@gmail.com>
2026-03-24 20:44:12 -0700

    basuraaa

---
+++ src/lib/mock-data.ts
@@ -0,0 +1,42 @@
+export const mockInsumos = [
+  {
+    id: 1,
+    codigo: 'INS-001',
+    nombre: 'Resina Composite Universal',
+    categoria: 'Materiales de restauración',
+    cantidadActual: 50,
+    unidadMedida: 'Pieza',
+    nivelMinimo: 20,
+    estadoStock: 'Normal'
+  },
+  {
+    id: 2,
+    codigo: 'INS-002',
+    nombre: 'Lidocaína 2% con Epinefrina',
+    categoria: 'Anestesia',
+    cantidadActual: 5,
+    unidadMedida: 'Caja',
+    nivelMinimo: 10,
+    estadoStock: 'Bajo stock'
+  },
+  {
+    id: 3,
+    codigo: 'INS-003',
+    nombre: 'Guantes de Látex (Medianos)',
+    categoria: 'Consumibles',
+    cantidadActual: 0,
+    unidadMedida: 'Caja',
+    nivelMinimo: 5,
+    estadoStock: 'Sin stock'
+  },
+  {
+    id: 4,
+    codigo: 'INS-004',
+    nombre: 'Rollos de Algodón',
+    categoria: 'Higiene',
+    cantidadActual: 15,
+    unidadMedida: 'Bolsa',
+    nivelMinimo: 10,
+    estadoStock: 'Normal'
+  }
+];
